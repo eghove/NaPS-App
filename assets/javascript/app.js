4f3949b8ab1a3160547b25d46ea73164b6d73dd1
That_Guy <eghove@gmail.com>
2018-11-01 17:32:23 -0500

    parser built

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -5,6 +5,11 @@ var ParkNames = [];
 var npsSearch = "Colorado";
 var npsURL = "http://developer.nps.gov/api/v1/parks?q=" + npsSearch + "&api_key=z3gukqYquzKbLQXkLJFI7OpTS88qyjCZV5DbjcHc";
 
+//array that captures the latitude and longitude of each NPS returned from the initial NPS ajax call
+let latLongParkData = [];
+
+//array that will capture the imagre URLs from NASA for the park results page
+
 //FUNCTIONS
 //===============================================================
 $.ajax({
@@ -18,12 +23,17 @@ $.ajax({
     for (var j = 0; j < ParkData.length; j++) {
         
         ParkNames.push("http://developer.nps.gov/api/v1/campgrounds?q=" + ParkData[j].fullName + "&api_key=z3gukqYquzKbLQXkLJFI7OpTS88qyjCZV5DbjcHc");
-    }
 
+        //push the latitude and longitude string from the above response into latLongParkData array
+        latLongParkData.push(ParkData[j].latLong);
+    }
+    latLongParser();
+    NASAQuery(latLongParkData[0][0], latLongParkData[0][1]);
     camping();
 });
 
-console.log(ParkNames);
+//console.log(ParkNames);
+console.log(latLongParkData);
 
 function camping() {
     console.log("hi");
@@ -31,10 +41,10 @@ function camping() {
     for(var i=0;i<ParkNames.length;i++) {
         
         $.ajax({
-            url: ParkNames[1],
+            url: ParkNames[1], //should this be [i] instead of [1]?
             method: "GET"
         }).then(function(response1) {
-            console.log(response1);
+            //console.log(response1);
         });
     };
 };
@@ -75,8 +85,9 @@ function weatherQuery(latitude, longitude){
 //      });
    
 }
+
 //basic NASA Satellinte Imagery API QUERY FUNCTION
-function NASAQuery (longitude, latitude) {
+function NASAQuery (latitude, longitude) {
     //NASA API Key
     const NASAAPIKey='z3gukqYquzKbLQXkLJFI7OpTS88qyjCZV5DbjcHc';
     //base NASA Imagery API
@@ -94,9 +105,42 @@ function NASAQuery (longitude, latitude) {
       })
       .then(function(response) {
           NASAImageURL = response.url;
+          console.log(NASAQueryURL);
           console.log(NASAImageURL);
       });
 };
 
+//latitute, longitude parser
+//takes the latitude and longitude from the NPS API and turns it into a value the other APIs can use
+function latLongParser() {
+    for (var k=0; k < latLongParkData.length; k++ ) {
+        //set a itemToConvert to the latitude longitude string
+        let itemToConvert = latLongParkData[k];
+        //remove "lat:" from itemToConvert
+        itemToConvert = itemToConvert.replace("lat:","");
+        //remove " long:" from itemToConvert (there is a space before 'long')
+        itemToConvert = itemToConvert.replace(" long:","");
+        //turn the itemToConvert string to an array of 2 items
+        itemToConvert = itemToConvert.split(",");
+        //for loop that turns itemToConvert to values NASA API and OpenWeather API can use
+        for (var l=0; l < itemToConvert.length; l++) {
+            //set latitude or longitude to the variable value
+            let value = itemToConvert[l];
+            //transform the string to a floating point decimal
+            value = parseFloat(value);
+            //transform the fp decimal above to a string with two decimals
+            value = value.toFixed(2);
+            //put the new converted value back into itemToConvert array
+            itemToConvert[l] = value;
+        }
+
+        //replaces the items in the original latLongParkData array with the converted strings
+        latLongParkData[k] = itemToConvert;
+        console.log(latLongParkData);
+    }
+};
  //MAIN PROCESSES
  //===============================================================
+
+ 
+ 
