25a0aca9aa42acd21eabeb0752ec4c2860cc2086
Andrew Burns <andrewburns@stp-er-10-129-106-6.wireless.umn.edu>
2018-11-03 11:12:09 -0500

    Don't merge

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,39 +1,39 @@
-var ParkNames = [];
+// var ParkNames = [];
 
-var npsSearch = "Colorado";
-var npsURL = "http://developer.nps.gov/api/v1/parks?q=" + npsSearch + "&api_key=z3gukqYquzKbLQXkLJFI7OpTS88qyjCZV5DbjcHc";
+// var npsSearch = "Colorado";
+// var npsURL = "http://developer.nps.gov/api/v1/parks?q=" + npsSearch + "&api_key=z3gukqYquzKbLQXkLJFI7OpTS88qyjCZV5DbjcHc";
 
-$.ajax({
-    url: npsURL,
-    method: "GET"
-}).then(function (response) {
-    console.log(response);
+// $.ajax({
+//     url: npsURL,
+//     method: "GET"
+// }).then(function (response) {
+//     console.log(response);
 
-    var ParkData = response.data;
+//     var ParkData = response.data;
 
-    for (var j = 0; j < ParkData.length; j++) {
+//     for (var j = 0; j < ParkData.length; j++) {
         
-        ParkNames.push("http://developer.nps.gov/api/v1/campgrounds?q=" + ParkData[j].fullName + "&api_key=z3gukqYquzKbLQXkLJFI7OpTS88qyjCZV5DbjcHc");
-    }
+//         ParkNames.push("http://developer.nps.gov/api/v1/campgrounds?q=" + ParkData[j].fullName + "&api_key=z3gukqYquzKbLQXkLJFI7OpTS88qyjCZV5DbjcHc");
+//     }
 
-    camping();
-});
+//     camping();
+// });
 
-console.log(ParkNames);
+// console.log(ParkNames);
 
-function camping() {
-    console.log("hi");
+// function camping() {
+//     console.log("hi");
 
-    for(var i=0;i<ParkNames.length;i++) {
+//     for(var i=0;i<ParkNames.length;i++) {
         
-        $.ajax({
-            url: ParkNames[1],
-            method: "GET"
-        }).then(function(response1) {
-            console.log(response1);
-        });
-    };
-};
+//         $.ajax({
+//             url: ParkNames[1],
+//             method: "GET"
+//         }).then(function(response1) {
+//             console.log(response1);
+//         });
+//     };
+// };
 
 
 
@@ -128,54 +128,54 @@ function camping() {
 
 
 
-function weatherQuery(latitude, longitude){
-    // open weather api key
+// function weatherQuery(latitude, longitude){
+//     // open weather api key
 
-    var weatherAPIkey = "e0517042c4c62f6d8cc8a258ba9ed1b4";
+//     var weatherAPIkey = "e0517042c4c62f6d8cc8a258ba9ed1b4";
 
-    // open weather base url
+//     // open weather base url
 
-    var weatherBaseURL = "http://api.openweathermap.org/data/2.5/weather?"
+//     var weatherBaseURL = "http://api.openweathermap.org/data/2.5/weather?"
 
-    var latitudeParam = latitude;
+//     var latitudeParam = latitude;
 
-    var longitudeParam = longitude;
+//     var longitudeParam = longitude;
 
-    // setting up the query url
-    var weatherQueryURL = weatherBaseURL + "lat=" + latitudeParam + "&lon=" + longitudeParam + "&appid=" + weatherAPIkey;
-    console.log(weatherQueryURL);
+//     // setting up the query url
+//     var weatherQueryURL = weatherBaseURL + "lat=" + latitudeParam + "&lon=" + longitudeParam + "&appid=" + weatherAPIkey;
+//     console.log(weatherQueryURL);
     
-   $.ajax({
-       URL: weatherQueryURL,
-       method: "GET"
-     }).then(function(response){
-         console.log(response);
-         currentweatherURL = response.main.temp;
-         console.log("Temp:" + currentweatherURL);
+//    $.ajax({
+//        URL: weatherQueryURL,
+//        method: "GET"
+//      }).then(function(response){
+//          console.log(response);
+//          currentweatherURL = response.main.temp;
+//          console.log("Temp:" + currentweatherURL);
 
-     });
+//      });
    
-}
-//basic NASA Satellinte Imagery API QUERY FUNCTION
-function NASAQuery (longitude, latitude) {
-    //NASA API Key
-    const NASAAPIKey='z3gukqYquzKbLQXkLJFI7OpTS88qyjCZV5DbjcHc';
-    //base NASA Imagery API
-    const NASABaseURL = 'https://api.nasa.gov/planetary/earth/imagery?';
-    let longitudeParam = longitude;
-    let latitudeParam = latitude;
-    //setting up the query url
-    let NASAQueryURL = NASABaseURL + 'lon=' + longitudeParam + '&lat=' + latitudeParam + '&api_key=' + NASAAPIKey ;
-    //console.log(NASAQueryURL);
-    let NASAImageURL='';
-    //the ajax call
-    $.ajax({
-        url: NASAQueryURL,
-        method: "GET"
-      })
-      .then(function(response) {
-          NASAImageURL = response.url;
-          console.log(NASAImageURL);
-      });
-};
+// }
+// //basic NASA Satellinte Imagery API QUERY FUNCTION
+// function NASAQuery (longitude, latitude) {
+//     //NASA API Key
+//     const NASAAPIKey='z3gukqYquzKbLQXkLJFI7OpTS88qyjCZV5DbjcHc';
+//     //base NASA Imagery API
+//     const NASABaseURL = 'https://api.nasa.gov/planetary/earth/imagery?';
+//     let longitudeParam = longitude;
+//     let latitudeParam = latitude;
+//     //setting up the query url
+//     let NASAQueryURL = NASABaseURL + 'lon=' + longitudeParam + '&lat=' + latitudeParam + '&api_key=' + NASAAPIKey ;
+//     //console.log(NASAQueryURL);
+//     let NASAImageURL='';
+//     //the ajax call
+//     $.ajax({
+//         url: NASAQueryURL,
+//         method: "GET"
+//       })
+//       .then(function(response) {
+//           NASAImageURL = response.url;
+//           console.log(NASAImageURL);
+//       });
+// };
 
